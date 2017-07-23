af524d17cfbb8f600dc39fecc0e42d0626a729f1
Joseph M. O'Connor <tiluser@yahoo.com>
2017-07-22 21:03:02 -0400

    Commit to allow switching to master

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,14 +9,17 @@
         $stateProvider
             .state('landing', {
                 url: '/',
+                controller: 'LandingCtrl as landing',
                 templateUrl: '/templates/landing.html'
           })
             .state('collection', {
               url: '/collection',
+              controller: 'CollectionCtrl as collection',
               templateUrl: '/templates/collection.html'
           })
             .state('album', {
               url: '/album',
+              controller: 'AlbumCtrl as album',
               templateUrl: '/templates/album.html'
           });
     }
