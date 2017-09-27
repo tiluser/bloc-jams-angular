ddab1a7c7b36d0105274911381fe21d389291dbb
Joseph M. O'Connor <tiluser@yahoo.com>
2017-09-27 09:29:45 -0400

    Add remove buttons to playlist

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,15 @@
 (function () {
-    function AlbumCtrl(Fixtures, SongPlayer) {
+    function AlbumCtrl(Fixtures, SongPlayer, $window, $scope) {
         this.albumData = Fixtures.getAlbum();
         this.songPlayer = SongPlayer;
+        $scope.test = function(text) {
+            $window.alert(text);
+        }
+        $scope.removeSong = function (songs, songIndex) {
+            songs.splice(songIndex,1);
+        };
     }
     angular
         .module("blocJams")
-        .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
+        .controller('AlbumCtrl', ['Fixtures', 'SongPlayer','$window', '$scope', AlbumCtrl]);
 })();
